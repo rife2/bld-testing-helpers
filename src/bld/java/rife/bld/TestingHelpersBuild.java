ab0951d8b15f1ef9e7d9e6a30aea548388c34d12
Erik C. Thauvin <erik@thauvin.net>
2026-09-08 12:34:00 -0700

    Version 1.1.0

--- src/bld/java/rife/bld/TestingHelpersBuild.java
+++ src/bld/java/rife/bld/TestingHelpersBuild.java
@@ -37,7 +37,7 @@ public class TestingHelpersBuild extends Project {
         pkg = "rife.bld";
         name = "Testing Helpers";
         archiveBaseName = "bld-testing-helpers";
-        version = version(1, 1, 0, "SNAPSHOT");
+        version = version(1, 1, 0);
 
         javaRelease = 17;
 
@@ -61,7 +61,7 @@ public class TestingHelpersBuild extends Project {
                 .include(junitJupiter)
                 .include(junitPlatform)
                 .include(dependency("io.github.classgraph", "classgraph",
-                        version(4, 8, 194)))
+                        version(4, 8, 195)))
                 .include(dependency("org.mockito", "mockito-junit-jupiter",
                         version(5, 23, 0)));
 
