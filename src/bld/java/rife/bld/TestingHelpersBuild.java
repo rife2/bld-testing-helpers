6af4a9ee91e0d54bddfae1cd6534f31af3870686
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 23:49:59 -0700

    Bump bld to version 3.0.0

--- src/bld/java/rife/bld/TestingHelpersBuild.java
+++ src/bld/java/rife/bld/TestingHelpersBuild.java
@@ -37,7 +37,7 @@ public class TestingHelpersBuild extends Project {
         pkg = "rife.bld";
         name = "Testing Helpers";
         archiveBaseName = "bld-testing-helpers";
-        version = version(1, 1, 0);
+        version = version(1, 1, 1, "SNAPSHOT");
 
         javaRelease = 17;
 
